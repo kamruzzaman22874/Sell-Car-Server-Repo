bc27849673924a24aed7b6b85b2dd2cac4d462bb
kamruzzaman22874 <joybd.eee@gmail.com>
2022-11-28 16:40:55 +0600

    Get car collection

--- index.js
+++ index.js
@@ -21,6 +21,41 @@ async function run() {
     try {
      const carCollection = client.db("UsedCar").collection("addedCars")
 
+        app.get('/allseller' , async(req , res)=>{
+            const email = req.query.email;
+            const query = {email : email}
+        })
+
+
+
+        	//!=====START======Company wise data load.......======>
+
+	app.get('/corolla', async (req, res) => {
+		const query = { name: 'Corolla' };
+		result = await carCollection.find(query).toArray();
+		res.send(result);
+		// res.send({name:'wroking'})
+	});
+	app.get('/tesla', async (req, res) => {
+		result = await carCollection
+			.find({ name: 'Tesla' })
+			.toArray();
+		res.send(result);
+		// res.send({name:'wroking'})
+	});
+	app.get('/pajeroo', async (req, res) => {
+		const query = { name: 'Pajeroo' };
+		result = await carCollection.find(query).toArray();
+		res.send(result);
+		// res.send({name:'wroking'})
+	});
+	//!=====END======!
+
+
+
+
+
+
      app.post('/addedproducts', async(req, res) => {
         const products = req.body
         const result = await carCollection.insertOne(products);
